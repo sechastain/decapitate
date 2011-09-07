573a4d360e073794212ec17c4e86313402f33ef9
Spencer Chastain <sechastain@gmail.com>
2011-09-06 20:21:17 -0400

    Trying to include jquery into my bookmarklet

--- decapitate.js
+++ decapitate.js
@@ -1,3 +1,7 @@
 (function() {
+var s=document.createElement('script');
+s.setAttribute('src','http://jquery.com/src/jquery-latest.js');
+document.getElementsByTagName('body')[0].appendChild(s);
 window.alert("Hello, World!!");
+$('body').background('#000000');
 })();
