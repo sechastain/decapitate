f484916ea9546735f0ef75b575174604b5748025
Spencer Chastain <sechastain@gmail.com>
2011-09-06 22:17:35 -0400

    Another jQuery tweak

--- decapit.js
+++ decapit.js
@@ -2,20 +2,20 @@
   function SearchForCaps(el)
   {
     var children = el.children();
-    if(children.size() == 0)
+    if(children.size() > 0)
     {
+      var i = 0;
+      for(i = 0; i < children.size(); i++) SearchForCaps($(children[i]));
+    }
+    //else
+    //{
       var t = el.text();
       var u = t.toUpperCase();
       if(t != u)
       {
         el.text(u);
       }
-    }
-    else
-    {
-      var i = 0;
-      for(i = 0; i < children.size(); i++) SearchForCaps($(children[i]));
-    }
+    //}
   }
 SearchForCaps($(document));
 })();
