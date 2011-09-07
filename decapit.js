016934aa42f55b70cfd8daa6ad2657918262e70d
Spencer Chastain <sechastain@gmail.com>
2011-09-06 20:54:53 -0400

    Fun with jQuery\!

--- decapit.js
+++ decapit.js
@@ -1,3 +1,20 @@
 (function() {
-$(':text').css({background:'yellow'});
+  function SearchForCaps(el)
+  {
+    var children = el.children();
+    if(children.size() == 0)
+    {
+      var t = el.text();
+      var u = t.upper();
+      if(t != u)
+      {
+        el.text(u);
+      }
+    }
+    else
+    {
+      children.each(SearchForCaps);
+    }
+  }
+SearchForCaps($());
 })();
