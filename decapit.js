d0b35046cf09adae0424dde132e554bc9d15b2c4
Spencer Chastain <sechastain@gmail.com>
2011-09-07 21:23:08 -0400

    Still trying to avoid script tag

--- decapit.js
+++ decapit.js
@@ -1,7 +1,7 @@
 (function() {
   function SearchForCaps()
   {
-    if(this.tagName.toUpperCase() === 'SCRIPT') return;
+    if(this.nodeType == 1 && this.tagName.toUpperCase() === 'SCRIPT') return;
     var el = $(this);
     var contents = el.contents();
     if(contents.size() == 0)
