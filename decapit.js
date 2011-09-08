8719ec776b649ea20e246dc8c095c663e82dc777
Spencer Chastain <sechastain@gmail.com>
2011-09-07 22:34:53 -0400

    Ignore style tags

--- decapit.js
+++ decapit.js
@@ -1,7 +1,7 @@
 (function() {
   function SearchForCaps()
   {
-    if(this.nodeType == 1 && this.tagName.toUpperCase() === 'SCRIPT') return;
+    if(this.nodeType == 1 && (this.tagName.toUpperCase() === 'SCRIPT' || this.tagName.toUpperCase() == 'STYLE')) return;
     var el = $(this);
     var contents = el.contents();
     if(contents.size() == 0)
