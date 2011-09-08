06a8e12af43bb046b8e4155cc5d2d17693a3e808
Spencer Chastain <sechastain@gmail.com>
2011-09-07 20:26:25 -0400

    Using contents instead of content

--- decapit.js
+++ decapit.js
@@ -2,8 +2,8 @@
   function SearchForCaps()
   {
     var el = $(this)
-    var content = el.content();
-    if(content.size() == 0)
+    var contents = el.contents();
+    if(contents.size() == 0)
     {
       var t = el.text();
       var u = t.toUpperCase();
@@ -14,7 +14,7 @@
     }
     else
     {
-      content.each(SearchForCaps);
+      contents.each(SearchForCaps);
     }
   }
 $('body').each(SearchForCaps);
